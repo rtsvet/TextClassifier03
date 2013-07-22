a81e0c6c89e441bd7178ba5c05cd19ec40e58e87
Rado Tsvetkov <radoslav.tsvetkov@cortalconsors.de>
2013-07-22 16:02:27 +0200

    clusterer and classifier

--- src/textclassifier03/TextClassifier03.java
+++ src/textclassifier03/TextClassifier03.java
@@ -14,9 +14,13 @@ import weka.core.Debug;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.SelectedTag;
+import weka.core.stemmers.SnowballStemmer;
+import weka.core.stemmers.Stemmer;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
 import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.gui.beans.ClassifierPerformanceEvaluator;
 
 public class TextClassifier03 {
 
@@ -27,7 +31,7 @@ public class TextClassifier03 {
      * 
      */
     public static void main(final String[] args) throws Exception {
-        System.out.println("Running");
+        System.out.println("Running Classifier");
 
         // Read Train Data
         Instances dataTrain = new Instances(new BufferedReader(new FileReader(DATA_TRAIN_FILE)));
@@ -39,8 +43,10 @@ public class TextClassifier03 {
 
         StringToWordVector str2Vec = new StringToWordVector();
         // Set Filter Parameters
-        //str2Vec.setOptions(new String[] {"-N 1" });
+        str2Vec.setOptions(new String[]{"-N 1"});
         str2Vec.setLowerCaseTokens(true);
+        str2Vec.setIDFTransform(true);
+        str2Vec.setStemmer(new SnowballStemmer());
         str2Vec.setInputFormat(dataTrain);
         // Instances fiteredTrainData = Filter.useFilter(dataTrain, str2Vec);
 
@@ -62,21 +68,37 @@ public class TextClassifier03 {
             System.out.println(", predicted: " + dataTest.classAttribute().value((int) pred));
         }
 
-
-        /*
-
-        EM clusterer = new EM();
-        clusterer.setNumClusters(3);
-        // set further options for EM, if necessary...
-        clusterer.buildClusterer(fiteredTrainData);
-        System.out.println(fiteredTrainData);
-
-        ClusterEvaluation eval = new ClusterEvaluation();
-        eval.setClusterer(clusterer);
-        eval.evaluateClusterer(dataTrain);
-
         // evaluate classifier and print some statistics
-        System.out.println(eval.clusterResultsToString());
-        */ 
+        Evaluation eval = new Evaluation(dataTrain);
+        eval.evaluateModel(fc, dataTest);
+        System.out.println(eval.toSummaryString("\nResults\n======\n", false));
+
+        System.out.println("Running Clusterer");
+
+        Remove filter = new Remove();
+        filter.setAttributeIndices("" + (dataTrain.classIndex() + 1));
+        filter.setInputFormat(dataTrain);
+        Instances classRemovedData = Filter.useFilter(dataTrain, filter);
+
+        str2Vec.setInputFormat(classRemovedData);
+        Instances fiteredTrainData = Filter.useFilter(classRemovedData, str2Vec);
+
+         EM clusterer = new EM();
+         clusterer.setNumClusters(2);
+         
+         // set further options for EM, if necessary...
+         clusterer.buildClusterer(fiteredTrainData);
+         
+         ClusterEvaluation clEval = new ClusterEvaluation();
+         clEval.setClusterer(clusterer);
+         
+         str2Vec.setInputFormat(dataTrain);
+         Instances strVectorTrainData = Filter.useFilter(dataTrain, str2Vec);
+         strVectorTrainData.setClassIndex(0);
+         clEval.evaluateClusterer(strVectorTrainData);
+
+         // evaluate classifier and print some statistics
+         System.out.println(clEval.clusterResultsToString());
+         
     }
 }
