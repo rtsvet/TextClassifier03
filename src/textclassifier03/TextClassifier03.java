b89c9abd3aef4a87535d8d303dcbc8b2a49ef530
Rado Tsvetkov <radoslav.tsvetkov@cortalconsors.de>
2013-07-23 11:06:27 +0200

    cluster and centroids

--- src/textclassifier03/TextClassifier03.java
+++ src/textclassifier03/TextClassifier03.java
@@ -2,6 +2,15 @@ package textclassifier03;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.SMO;
@@ -9,6 +18,7 @@ import weka.classifiers.meta.FilteredClassifier;
 import weka.classifiers.trees.J48;
 import weka.clusterers.ClusterEvaluation;
 import weka.clusterers.EM;
+import weka.clusterers.SimpleKMeans;
 import weka.core.Attribute;
 import weka.core.Debug;
 import weka.core.FastVector;
@@ -26,6 +36,7 @@ public class TextClassifier03 {
 
     private static final String DATA_TRAIN_FILE = "/export/Development/DataMining/Weka/weka-3-6-9/data/ReutersCorn-train.arff";
     private static final String DATA_TEST_FILE = "/export/Development/DataMining/Weka/weka-3-6-9/data/ReutersCorn-test.arff";
+
 
     /*
      * 
@@ -83,22 +94,54 @@ public class TextClassifier03 {
         str2Vec.setInputFormat(classRemovedData);
         Instances fiteredTrainData = Filter.useFilter(classRemovedData, str2Vec);
 
-         EM clusterer = new EM();
-         clusterer.setNumClusters(2);
-         
-         // set further options for EM, if necessary...
-         clusterer.buildClusterer(fiteredTrainData);
-         
-         ClusterEvaluation clEval = new ClusterEvaluation();
-         clEval.setClusterer(clusterer);
-         
-         str2Vec.setInputFormat(dataTrain);
-         Instances strVectorTrainData = Filter.useFilter(dataTrain, str2Vec);
-         strVectorTrainData.setClassIndex(0);
-         clEval.evaluateClusterer(strVectorTrainData);
+        //EM clusterer = new EM();
+        SimpleKMeans clusterer = new SimpleKMeans();
 
-         // evaluate classifier and print some statistics
-         System.out.println(clEval.clusterResultsToString());
-         
+        clusterer.setNumClusters(2);
+
+        // set further options for EM, if necessary...
+        clusterer.buildClusterer(fiteredTrainData);
+
+        System.out.println("Clusters:");
+
+        // For EM clusterer
+        //double[] clPrios = clusterer.clusterPriors();
+        //for (int i = 0; i < clPrios.length; i++) {
+        //   System.out.print(i + "=" + clPrios[i]+"; ");
+        //}
+
+        // for K-Means
+        //NavigableMap<String, Double> centroMap = new TreeMap<String, Double>();
+        Map<String, Double> centroMap = new LinkedHashMap<String, Double>();
+
+        Instances centroids = clusterer.getClusterCentroids();
+        for (int i = 0; i < centroids.size(); i++) {
+            System.out.println("Cluster " + i);
+            Instance cen = centroids.get(i);
+            for (int j = 0; j < cen.numAttributes(); j++) {
+                centroMap.put(cen.attribute(j).name(), cen.value(j));
+            }
+            centroMap = MapUtil.sortByValue(centroMap);
+            int k = 0;
+            for (Iterator<Map.Entry<String, Double>> it = centroMap.entrySet().iterator(); it.hasNext();) {
+                System.out.println(it.next() +"; ");
+                k++;
+                if (k > 10) break;
+            }
+            System.out.println("End ");
+            centroMap.clear();
+        }
+
+        ClusterEvaluation clEval = new ClusterEvaluation();
+        clEval.setClusterer(clusterer);
+
+        str2Vec.setInputFormat(dataTrain);
+        Instances strVectorTrainData = Filter.useFilter(dataTrain, str2Vec);
+        strVectorTrainData.setClassIndex(0);
+        clEval.evaluateClusterer(strVectorTrainData);
+
+        // evaluate classifier and print some statistics
+        //System.out.println(clEval.clusterResultsToString());
+
     }
 }
